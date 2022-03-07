72342c61644a0253c2ecd65d4fe0f4f1a8290c5d
Petzr <peterjansen812@gmail.com>
2022-03-07 15:06:19 +0100

    created classes

---
+++ src/com/project/Examen.java
@@ -0,0 +1,20 @@
+package com.project;
+
+import java.util.ArrayList;
+
+public class Examen {
+    ArrayList<Vraag> vragen = new ArrayList<>();
+
+
+}
+
+class Vraag {
+    String vraag;
+    Antwoord correctAntwoord;
+    ArrayList<Antwoord> mogelijke_antwoorden = new ArrayList<>();
+
+}
+
+class Antwoord {
+
+}
