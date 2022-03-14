5835d001a16722a4a85213facaffd38264943174
daner <daner.elias@gmail.com>
2022-03-14 14:43:06 +0100

    Menu in de main updated: Text voor de keuze

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -8,7 +8,16 @@ public class Main {
     public static void menu() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Kies optie: ");
+        System.out.println("1) Lijst met examens\n" +
+                "2) Lijst met studenten\n" +
+                "3) Nieuwe student inschrijven\n" +
+                "4) Student verwijderen\n" +
+                "5) Examen afnemen\n" +
+                "6) Is student geslaagd voor test?\n" +
+                "7) Welke examens heeft student gehaald?\n" +
+                "8) Welke student heeft de meeste examens gehaald?\n" +
+                "0) Exit\n" +
+                "Uw keuze:\n");
 
     }
 
