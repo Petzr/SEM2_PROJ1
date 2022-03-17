48db3af3870ee1ec00c1130326666cec85c7a275
daner <daner.elias@gmail.com>
2022-03-17 14:33:20 +0100

    Class updated!

--- src/com/project/School.java
+++ src/com/project/School.java
@@ -1,6 +1,7 @@
 package com.project;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class School {
     ArrayList<Examen> alleExamens = new ArrayList<>();
@@ -15,6 +16,31 @@ public class School {
         for (Student student : array)
         {
             System.out.println(student.getStudentenNummer() + " " + student.getNaam());
+        }
+    }
+
+    public static void nieuwStudent(ArrayList<Student> array)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Geef naam: ");
+        String naam = scanner.nextLine();
+        Integer nummer = StudentNummer.getNummer();
+        array.add(new Student(naam, nummer));
+        System.out.println("Student toegevoegt!");
+    }
+
+    public static void verwijderStudent(ArrayList<Student> array) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Geef het studentnummer: ");
+        int nummer = scanner.nextInt();
+        for (int i = 0; i < array.size(); i++)
+        {
+            if (nummer == array.get(i).getStudentenNummer())
+            {
+                System.out.println(array.get(i).getNaam() + " verwijdert.");
+                array.remove(i);
+                break;
+            } else {}
         }
     }
 }
