d146a1f4d9058ac2b719e1e76b6e3fdd2e044c60
Petzr <peterjansen812@gmail.com>
2022-03-21 12:45:11 +0100

    added tests voor class Examen

---
+++ Tests/com/project/ExamenTest.java
@@ -0,0 +1,28 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamenTest {
+
+    @Test
+    void getNaam() {
+    }
+
+    @Test
+    void getVragen() {
+    }
+
+    @Test
+    void getAantalVragen() {
+    }
+
+    @Test
+    void examenAfnemen() {
+    }
+
+    @Test
+    void isGeslaagdVoorExamen() {
+    }
+}
