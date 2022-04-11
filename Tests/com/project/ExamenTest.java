09eb9d15f11eaa24a8aa08376a9bd56df5ed481f
Petzr <peterjansen812@gmail.com>
2022-04-11 20:23:41 +0200

    test verbetering

--- Tests/com/project/ExamenTest.java
+++ Tests/com/project/ExamenTest.java
@@ -28,16 +28,18 @@ class ExamenTest {
 
     @Test
     public void toonExamenInfoTest() {
+        // arrange
         Examen wiskunde = new Examen("Resources/WiskundeExamenVragen.txt");
         Examen engels = new Examen("Resources/EngelsExamenVragen.txt");
-
 
         String wiskundeResult = "Examen naam: WiskundeExamenVragen.txt, aantal vragen bij examen: 10";
         String engelsResult = "Examen naam: EngelsExamenVragen.txt, aantal vragen bij examen: 10";
 
+        // act
         String WiskundeFormat = String.format("Examen naam: %s, aantal vragen bij examen: %d", wiskunde.getNaam(), wiskunde.getVragen().size());
         String engelsFormat = String.format("Examen naam: %s, aantal vragen bij examen: %d", engels.getNaam(), engels.getVragen().size());
 
+        // assert
         assertTrue(wiskundeResult.equals(WiskundeFormat));
         assertTrue(engelsResult.equals(engelsFormat));
 
@@ -51,29 +53,33 @@ class ExamenTest {
  }
 
     @Test
-    void isGeslaagdVoorExamenTest() {
+    void isGeslaagdVoorExamenTest() { // ik(Peter) snap niet waarom je het zo lastig voor jezelf hebt gemaakt
+        // arrange
         boolean expected1 = true;
         boolean expected2 = false;
         boolean actual1;
         boolean actual2;
 
+        // act
         if (8 > 10 / 2) {
             System.out.println("Goed gedaan, je hebt het examen gehaald.");
             actual1 = true;
         } else {
             System.out.println("Helaas, je hebt het examen niet gehaald.");
             actual2 = false;
-
-            if (4 > 10 / 2) {
-                System.out.println("Goed gedaan, je hebt het examen gehaald.");
-                actual1 = true;
-            } else {
-                System.out.println("Helaas, je hebt het examen niet gehaald.");
-                actual2 = false;
-
-            }
-            assertEquals(expected1, actual1);
-            assertEquals(expected2, actual2);
         }
+
+        if (4 > 10 / 2) {
+            System.out.println("Goed gedaan, je hebt het examen gehaald.");
+            actual1 = true;
+        } else {
+            System.out.println("Helaas, je hebt het examen niet gehaald.");
+            actual2 = false;
+
+        }
+
+        // act
+        assertEquals(expected1, actual1);
+        assertEquals(expected2, actual2);
     }
 }
