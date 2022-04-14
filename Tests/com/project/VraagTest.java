077a78841b0d95d9d6f7d98aae5ee5387f22cd3c
Petzr <peterjansen812@gmail.com>
2022-04-14 15:01:35 +0200

    beantwoordVraagP() voor vergelijking met beantwoordVraag() van Daner

--- Tests/com/project/VraagTest.java
+++ Tests/com/project/VraagTest.java
@@ -2,6 +2,8 @@ package com.project;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,5 +36,18 @@ class VraagTest {
 
         assertTrue(actualGoed);
         assertEquals(actualFout, false);
+    }
+
+    @Test
+    void beantwoordVraagP() {
+        Vraag vraag = new Vraag("2 + 2", "4");
+
+        int expected = 1;
+
+        InputStream inputStream = new ByteArrayInputStream("4".getBytes());
+        System.setIn(inputStream);
+
+        assertEquals(expected, vraag.beantwoordVraag());
+
     }
 }
